8ae26a4e72dc0c2bee234061ca9dda62605606fb
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-05-14 01:38:52 +0300

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
 function ProjectList({ projects }) {
   console.log(projects);
+  
+  const projectsList = projects.map((aproject)=>{
+    console.log(aproject)
+
+    return <ProjectItem key = {aproject.id} name={aproject.name}  about= {aproject.about} technologies = {aproject.technologies}/>
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+    {projectsList}
+      </div>
     </div>
   );
 }
